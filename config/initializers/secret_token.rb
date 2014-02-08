c1866dea3763380d2c454eb0089300f594d59ac8
Paul Kwiatkowski <paul@groupraise.com>
2014-02-08 13:23:34 -0600

    Initial project commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AstronomWeb::Application.config.secret_key_base = 'c8baa9fc55b156d7dd9afab25a97897e28e7d8a7d21bac29c4bb1af11a1de6f1a46d8b32596eca449bd01368b8ed03253ac4e02f0958b18221783d5ad24056cf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AstronomWeb::Application.config.session_store :cookie_store, key: '_astronom-web_session'
